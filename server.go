9f5959c98375e8f0e4e07416433772145e9d88ec
Junkes887 <henriquejunkes887@gmail.com>
2020-06-24 19:55:57 -0300

    Arrays and Objects

--- server.go
+++ server.go
@@ -1,15 +1,35 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 )
 
+type User struct {
+	Name string
+	Age int
+}
+
 func main() {
-	http.HandleFunc("/", HelloServer)
+	// http.HandleFunc("/", helloServer)
+	http.HandleFunc("/users", listemUsers)
     http.ListenAndServe(":8080", nil)
 }
 
-func HelloServer(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w,"Hello!")
+// func helloServer(w http.ResponseWriter, r *http.Request) {
+//     fmt.Fprintf(w,"Hello!")
+// }
+
+func listemUsers(w http.ResponseWriter, r *http.Request) {
+	// var listUser [2]ListUser
+	var user2 = User{"Henrique",18}
+	var user1 = User{"Eder",43}
+	listUser := [2]User{user1, user2}
+	js, err := json.Marshal(listUser)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
 }
